41d034d2508efed3a9f2433e61d37ca3c17f2268
Toheeb Odusoga <78192323+Dreaprince@users.noreply.github.com>
2024-09-02 10:26:16 +0100

    update documentation

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -2,15 +2,20 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
-    @ApiProperty({ description: 'email'})
-    @IsNotEmpty({ message: 'email is required.' })
-    @IsString({ message: 'email must be a string.' })
-    @IsEmail({}, { message: 'email must be a valid email address.' })
-    email: string;
+  @ApiProperty({
+    description: 'The email address of the user',
+    example: 'user@example.com',
+  })
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsString({ message: 'Email must be a string.' })
+  @IsEmail({}, { message: 'Email must be a valid email address.' })
+  email: string;
 
-    @ApiProperty({ description: 'password' })
-    @IsNotEmpty({ message: 'password is required.' })
-    @IsString({ message: 'password must be a string.' })
-    password: string;
-
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'P@ssw0rd',
+  })
+  @IsNotEmpty({ message: 'Password is required.' })
+  @IsString({ message: 'Password must be a string.' })
+  password: string;
 }
--- src/users/dto/register.dto.ts
+++ src/users/dto/register.dto.ts
@@ -2,20 +2,28 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class RegisterDto {
-    @ApiProperty({ description: 'name' })
-    @IsNotEmpty({ message: 'name is required.' })
-    @IsString({ message: 'name must be a string.' })
-    name: string;
+  @ApiProperty({
+    description: 'The full name of the user',
+    example: 'John Doe',
+  })
+  @IsNotEmpty({ message: 'Name is required.' })
+  @IsString({ message: 'Name must be a string.' })
+  name: string;
 
+  @ApiProperty({
+    description: 'The email address of the user',
+    example: 'user@example.com',
+  })
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsString({ message: 'Email must be a string.' })
+  @IsEmail({}, { message: 'Email must be a valid email address.' })
+  email: string;
 
-    @ApiProperty({ description: 'email' })
-    @IsNotEmpty({ message: 'email is required.' })
-    @IsString({ message: 'email must be a string.' })
-    @IsEmail({}, { message: 'email must be a valid email address.' })
-    email: string;
-
-    @ApiProperty({ description: 'password' })
-    @IsNotEmpty({ message: 'password is required.' })
-    @IsString({ message: 'password must be a string.' })
-    password: string;
+  @ApiProperty({
+    description: 'The password for the user account',
+    example: 'P@ssw0rd',
+  })
+  @IsNotEmpty({ message: 'Password is required.' })
+  @IsString({ message: 'Password must be a string.' })
+  password: string;
 }
